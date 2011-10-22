e3abff5ec03c41800285000bf9f8cccc33551f81
Lin Jen-Shin <godfat@godfat.org>
2011-10-22 13:12:44 +0800

    put single character up front and remove duplicates

--- cleanup.rb
+++ cleanup.rb
@@ -2,4 +2,15 @@
 
 puts($stdin.read.lines.map{ |l|
   l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|　/, ' ').squeeze(' ')
-}.sort.join)
+}.sort{ |lhs, rhs|
+  p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
+  if p0.size == 1 && p1.size == 1
+    lhs <=> rhs
+  elsif p0.size == 1
+    -1
+  elsif p1.size == 1
+    1
+  else
+    lhs <=> rhs
+  end
+}.uniq.join)

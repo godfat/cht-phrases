3d0ed2fc1189f9570374627544c3d25e7117e1ef
Lin Jen-Shin <godfat@godfat.org>
2011-10-22 13:29:13 +0800

    even more easier, and strip trailing whites

--- cleanup.rb
+++ cleanup.rb
@@ -1,20 +1,25 @@
 # encoding: utf-8
 
-result = File.open(ARGV[0]).lines.map{ |l|
-  l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ')
-}.sort{ |lhs, rhs|
-  p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
-  if p0.size == 1 && p1.size == 1
-    lhs <=> rhs
-  elsif p0.size == 1
-    -1
-  elsif p1.size == 1
-    1
-  else
-    lhs <=> rhs
-  end
-}.uniq.join
+def cleanup file
+  result = File.open(file).lines.map{ |l|
+    l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ').
+      gsub(/ \n$/, "\n")
+  }.sort{ |lhs, rhs|
+    p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
+    if p0.size == 1 && p1.size == 1
+      lhs <=> rhs
+    elsif p0.size == 1
+      -1
+    elsif p1.size == 1
+      1
+    else
+      lhs <=> rhs
+    end
+  }.uniq.join
 
-File.open(ARGV[0], 'w'){ |f|
-  f.puts(result)
-}
+  File.open(file, 'w'){ |f|
+    f.puts(result)
+  }
+end
+
+cleanup(ARGV.shift) until ARGV.empty?

054f6a29a7f49e6ff5288a614e5078b9d5a06718
Lin Jen-Shin <godfat@godfat.org>
2011-10-24 00:28:50 +0800

    tweak, ignore empty lines

--- cleanup.rb
+++ cleanup.rb
@@ -2,9 +2,9 @@
 
 def cleanup file
   result = File.open(file).lines.map{ |l|
-    l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ').
-      gsub(/ \n$/, "\n")
-  }.sort{ |lhs, rhs|
+    ll = l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ').strip
+    "#{ll}\n" unless ll.empty?
+  }.compact.sort{ |lhs, rhs|
     p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
     if p0.size == 1 && p1.size == 1
       lhs <=> rhs

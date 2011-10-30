f7b527dc34dd31789c78142e8c3a535e93a1e43d
Lin Jen-Shin <godfat@godfat.org>
2011-10-31 03:07:17 +0800

    sort not only han characters

--- lib/cht-phrases.rb
+++ lib/cht-phrases.rb
@@ -4,7 +4,7 @@ module ChtPhrases
   module_function
   def sort_phrases phrases
     phrases.reject{ |l| l.strip.empty? }.sort{ |lhs, rhs|
-      p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
+      p0, p1 = lhs[/^\S+/], rhs[/^\S+/]
       if p0.size == 1 && p1.size == 1
         lhs <=> rhs
       elsif p0.size == 1

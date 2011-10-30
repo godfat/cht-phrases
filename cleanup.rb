8be0d27bb49ab574bffec9b4b6c6d0fd52db51b9
Lin Jen-Shin <godfat@godfat.org>
2011-10-31 01:23:13 +0800

    make it more clear

--- cleanup.rb
+++ cleanup.rb
@@ -5,8 +5,8 @@ require './lib/cht-phrases'
 def cleanup file
   result = ChtPhrases.sort_phrases(File.open(file).lines.map{ |l|
     ll = l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ').strip
-    "#{ll}\n" unless ll.empty?
-  }).join
+    ll unless ll.empty?
+  }.compact).join("\n")
 
   File.open(file, 'w'){ |f|
     f.puts(result)

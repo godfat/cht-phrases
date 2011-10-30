737e57302cdde428bcdf88111d677d8682bb2f30
Lin Jen-Shin <godfat@godfat.org>
2011-10-31 01:15:47 +0800

    introduce lib/cht-phrases.rb to sort phrases

--- cleanup.rb
+++ cleanup.rb
@@ -1,21 +1,12 @@
 # encoding: utf-8
 
+require './lib/cht-phrases'
+
 def cleanup file
-  result = File.open(file).lines.map{ |l|
+  result = ChtPhrases.sort_phrases(File.open(file).lines.map{ |l|
     ll = l.gsub(/\-?\d\.\d/, '').gsub(/,|\t|ˉ|　/, ' ').squeeze(' ').strip
     "#{ll}\n" unless ll.empty?
-  }.compact.sort{ |lhs, rhs|
-    p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
-    if p0.size == 1 && p1.size == 1
-      lhs <=> rhs
-    elsif p0.size == 1
-      -1
-    elsif p1.size == 1
-      1
-    else
-      lhs <=> rhs
-    end
-  }.uniq.join
+  }).join
 
   File.open(file, 'w'){ |f|
     f.puts(result)
---
+++ lib/cht-phrases.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+module ChtPhrases
+  module_function
+  def sort_phrases phrases
+    phrases.compact.sort{ |lhs, rhs|
+      p0, p1 = lhs[/^\p{han}+/], rhs[/^\p{han}+/]
+      if p0.size == 1 && p1.size == 1
+        lhs <=> rhs
+      elsif p0.size == 1
+        -1
+      elsif p1.size == 1
+        1
+      else
+        lhs <=> rhs
+      end
+    }.uniq
+  end
+end
